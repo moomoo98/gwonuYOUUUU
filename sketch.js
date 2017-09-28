992623efe96783353238d87cdb398c2d1bfed314
moomoo98 <32293583+moomoo98@users.noreply.github.com>
2017-09-28 16:57:55 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 function setup() { 
 	createCanvas(windowWidth, windowHeight); 
-	background(235,255,100);
+	background(235,255,190);
 }
 function draw() {
 	if (mouseIsPressed) {
-		var size = random(20, 20);
+		var size = random(20, 30);
 		ellipse(mouseX, mouseY, size, size); 
 	}
 }
